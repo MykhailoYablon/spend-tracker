f4161b7710372eed53f247777c838f42595c5e1b
mihay <Yfc22nehwsz$>
2025-05-22 11:04:20 +0300

    Adding initial dao model layer

---
+++ app/src/main/java/com/example/spendtracker/dao/TransactionDao.kt
@@ -0,0 +1,21 @@
+package com.example.spendtracker.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.spendtracker.model.Transaction
+
+@Dao
+interface TransactionDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(transaction: Transaction)
+
+    @Delete
+    suspend fun delete(transaction: Transaction)
+
+    @Query("SELECT * FROM transactions ORDER BY date DESC")
+    fun getAllTransactions(): LiveData<List<Transaction>>
+}
--- app/src/main/java/com/example/spendtracker/model/Transaction.kt
+++ app/src/main/java/com/example/spendtracker/model/Transaction.kt
@@ -1,5 +1,8 @@
 package com.example.spendtracker.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "transactions")
 data class Transaction(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
